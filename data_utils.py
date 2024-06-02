9cc86d0c79d6b0e35ff78a18cb89b5d579d1c445
Yifan Lu <111810457+yl231@users.noreply.github.com>
2024-06-02 03:15:45 -0500

    Update data_utils.py

--- data_utils.py
+++ data_utils.py
@@ -88,4 +88,81 @@ def process_mquake_remastered_cf_6334(dataset, edit_num = 6334):
 
     return train_edited, test_unedited, test_edited, test_edited_overlap
     # parameter-based methods should train on train_edited then separately evaluate on test_unedited and test_edited with three separate acc reported.
-    # test_overlaped is included in test_edited, so no need to separatly
+    # test_overlaped is included in test_edited, so no need to separately
+
+
+def check_answer(edit_flag, instance, ans):
+    # Define answer and answer_alias keys based on edit_flag
+    answer = "answer"
+    answer_alias = "answer_alias"
+    if edit_flag:
+        answer = "new_" + answer
+        answer_alias = "new_" + answer_alias
+    
+    # Convert the answer and ans to upper case
+    ans_upper = ans.upper()
+    instance_answer_upper = instance[answer].upper()
+    
+    # Convert each alias to upper case for comparison
+    instance_answer_alias_upper = [alias.upper() for alias in instance[answer_alias]]
+    
+    # Return true if ans matches the answer or any of the aliases
+    return ans_upper == instance_answer_upper or ans_upper in instance_answer_alias_upper
+
+
+# Evaluation:
+def cal_accuracy(dataset, raw_answer_dict, use_6334=False):
+    if not use_6334:
+        acc_list = ["unedited_acc", "edited_acc"]
+    else:
+        acc_list = ["unedited_acc", "train_edited_acc", "test_train_overlap_edited_acc", "test_unique_edited_acc"]
+        
+    total = {}
+    correct = {}
+    for acc in acc_list:
+        total[acc] = set()
+        correct[acc] = set()
+    
+    for d in dataset:
+        if d['case_id'] not in raw_answer_dict.keys():
+            continue
+        edited_flag = raw_answer_dict[d['case_id']]['edited']
+        this_is_correct = any(check_answer(edited_flag, d, ans) for ans in raw_answer_dict[d['case_id']]['answers'])
+        caseid = d['case_id']
+        if use_6334:
+            labels_6334 = d['6334_split']
+            if 'train_edited' in labels_6334:
+                total['train_edited_acc'].add(caseid)
+                if this_is_correct:
+                    correct['train_edited_acc'].add(caseid)
+
+            if 'test_edited_unique' in labels_6334:
+                total['test_unique_edited_acc'].add(caseid)
+                if this_is_correct:
+                    correct['test_unique_edited_acc'].add(caseid)
+            else:
+                if 'test_edited' in labels_6334:
+                    total['test_train_overlap_edited_acc'].add(caseid)
+                    if this_is_correct:
+                        correct['test_train_overlap_edited_acc'].add(caseid)
+
+            if 'test_unedited' in labels_6334:
+                total['unedited_acc'].add(caseid)
+                if this_is_correct:
+                    correct['unedited_acc'].add(caseid)
+            
+        else:
+            if edited_flag:
+                total['edited_acc'].add(caseid)
+                if this_is_correct:
+                    correct['edited_acc'].add(caseid)
+            else:
+                total['unedited_acc'].add(caseid)
+                if this_is_correct:
+                    correct['unedited_acc'].add(caseid)
+    
+    result = {}
+    for acc in acc_list:
+        result[acc] = len(correct[acc]) / len(total[acc]+ 1e-8)
+        
+    return result, correct, total
